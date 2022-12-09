cadffa84d9b1ad09fe46dd7d34fe9937f4d7ea59
Ömer göçmen <omer_gocmen68@outlook.com>
2022-12-09 11:54:07 +0300

    first commit

--- Entities/Concrete/Product.cs
+++ Entities/Concrete/Product.cs
@@ -10,6 +10,7 @@ namespace Entities.Concrete
     public class Product:IEntity
     {
         public int ProductId { get; set; }
+        public int Price { get; set; }
         public int CategoryId { get; set; }
         public string ProductName { get; set; }
         public short UnitsInStock { get; set; }
