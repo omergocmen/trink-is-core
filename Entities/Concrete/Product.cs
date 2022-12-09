fa6cd50eb897fc5000125bb341e319530086e6b1
0Halley0 <hale_ino@hotmail.com>
2022-12-09 12:03:07 +0300

    deneme

--- Entities/Concrete/Product.cs
+++ Entities/Concrete/Product.cs
@@ -11,6 +11,7 @@ namespace Entities.Concrete
     {
         public int ProductId { get; set; }
         public int Price { get; set; }
+        public int Hale { get; set; }
         public int CategoryId { get; set; }
         public string ProductName { get; set; }
         public short UnitsInStock { get; set; }
